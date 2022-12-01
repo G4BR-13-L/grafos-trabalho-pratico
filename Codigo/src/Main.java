d6d879e035a4490f7ee1e6a44ec2ed8f0cc0ddd8
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-11-30 23:51:23 -0300

    doc(tarefas): Alterada a lista de tarefas do Main para o README

--- Codigo/src/Main.java
+++ Codigo/src/Main.java
@@ -6,26 +6,7 @@ import java.io.FileNotFoundException;
 import Codigo.src.Graph.Graph;
 import Codigo.src.GraphIO.GraphIO;
 
-/**
- * Entrega 1
- * * Desenvolver uma biblioteca para a manipulação de grafos, contendo:
- * 
- * * REPRESENTAÇÕES
- * * Gabriel - [ ✅ ] Representação de grafos utilizando Matriz de adjacência
- * * Gabriel - [ ✅ ] Representação de grafos utilizando Lista de adjacência
- * 
- * * MANIPULAÇÃO
- * * Funções básicas para manipulação de grafos em ambas representações, incluindo:
- * * Gabriel - [ ✅ ] Criação de um grafo com X v ́ertices (o n ́umero de v ́ertices deve ser inserido pelo usu ́ario)
- * * Gabriel - [ ✅ ] Criação e remoção de arestas
- * * [ ❌ ] Ponderação e rotulação de vertices
- * * [ ❌ ] Ponderação e rotulação de arestas
- * * [ ❌ ] Checagem de adjacência entre v ́ertices
- * * [ ❌ ] Checagem de adjacência entre arestas
- * * [ ❌ ] Checagem da adjacência de arestas
- * * [ ❌ ] Checagem da quantidade de vertices e arestas
- * * [ ❌ ] Checagem de grafo vazio e completo
- */
+
 public class Main {
     public static Graph graph1;
     public static Graph graph2;
